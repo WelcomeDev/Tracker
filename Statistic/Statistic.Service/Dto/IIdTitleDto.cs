3d1b120e7556ef83026d3cb5f189db2ee15a3ef8
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-10-27 14:22:27 +1000

    add: on statistic api config

---
+++ Statistic/Statistic.Service/Dto/IIdTitleDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Statistic.Service.Dto
+{
+    public interface IIdTitleDto
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ Statistic/Statistic.Service/Dto/Tag/TagDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Statistic.Service.Dto.Tag
+{
+    public class TagDto : IIdTitleDto
+    {
+        public Guid Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Title { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
