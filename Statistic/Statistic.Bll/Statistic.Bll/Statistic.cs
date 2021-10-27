f53e2c2e5909915194e4d652cbbe1c0996b81182
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-10-27 17:15:22 +1000

    add: on bll

---
+++ Statistic/Statistic.Bll/Statistic.Bll/Statistic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Auth.Di;
+
+using Statistic.Di;
+using Statistic.Di.Tag;
+
+namespace Statistic.Bll
+{
+    internal class Statistic : IStatistic
+    {
+        public IUserIdentity User => throw new NotImplementedException();
+
+        public ITag Tag { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Title { set => throw new NotImplementedException(); }
+
+        public DateTime Date => throw new NotImplementedException();
+
+        public double Value => throw new NotImplementedException();
+
+        public Guid Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        string IStatisticEssentialsData.Title => throw new NotImplementedException();
+
+        public bool Equals(IStatistic? other) => throw new NotImplementedException();
+    }
+}
---
+++ Statistic/Statistic.Bll/Statistic.Bll/Tag.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Statistic.Di.Tag;
+
+namespace Statistic.Bll.Tag
+{
+    internal class Tag : ITag
+    {
+        public string Title { get; set; }
+        public Guid Id { get; set; }
+
+        public bool Equals(ITag? other)
+        {
+            if (other is null)
+                return false;
+
+            return Title.Equals(other.Title);
+        }
+    }
+}
