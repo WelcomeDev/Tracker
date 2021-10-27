ce6a98a4cde639b73171b5cc4d7b60ae0f8b462e
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-10-27 14:22:27 +1000

    add: on statistic api config

--- WelcomeDev/WelcomeDev.Provider.Di/IGuid.cs
+++ WelcomeDev/WelcomeDev.Provider.Di/IGuid.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WelcomeDev.Provider.Di
+﻿namespace WelcomeDev.Provider.Di
 {
     public interface IGuid
     {
