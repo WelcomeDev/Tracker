68ce360ede0f481b680b15d6d2be5c18a3107d9b
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-10-27 15:40:40 +1000

    add: user

--- Auth/Auth.Di/IUser.cs
+++ Auth/Auth.Di/IUser.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using WelcomeDev.Provider.Di;
+
 namespace Auth.Di
 {
-    public interface IUser
+    public interface IUser : IGuid
     {
         string Username { get; }
 
