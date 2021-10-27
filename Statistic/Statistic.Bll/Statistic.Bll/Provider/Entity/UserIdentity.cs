9eae99e4e865f03a7b68733901535dfd2a5fac32
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-10-27 17:14:59 +1000

    add: db configuration

---
+++ Statistic/Statistic.Bll/Statistic.Bll/Provider/Entity/UserIdentity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Auth.Di;
+
+namespace Statistic.Bll.Provider.Entity
+{
+    [ComplexType]
+    internal class UserIdentity : IUserIdentity
+    {
+        public string Name { get; set; }
+
+        public Guid Id { get; set; }
+    }
+}
---
+++ Statistic/Statistic.Bll/Statistic.Bll/Provider/StatisticDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using Statistic.Bll.Provider.Entity;
+
+namespace Statistic.Bll
+{
+    internal class StatisticDbContext : DbContext
+    {
+        //public DbSet<TagEntity> Tags { get; set; }
+
+        public DbSet<StatisticEntity> Statistics { get; set; }
+
+        public StatisticDbContext()
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TagEntity>()
+                        .Property(x => x.Id)
+                        .HasDefaultValueSql("NEWID()");
+
+            modelBuilder.Entity<StatisticEntity>()
+                       .Property(x => x.Id)
+                       .HasDefaultValueSql("NEWID()");
+
+            modelBuilder.Entity<StatisticEntity>()
+                       .Property(x => x.Date)
+                       .HasDefaultValueSql("GETDATE()");
+        }
+    }
+}
