b6f7ff0ab01e7714a0efc728f32abaad2dcd4b9b
galoperidol08@gmail.com <galoperidol08@gmail.com>
2021-11-04 01:12:32 +1000

    add: pomodoro controller API

--- Pomodoro/Pomodoro.Service/Controllers/PomodoroController.cs
+++ Pomodoro/Pomodoro.Service/Controllers/PomodoroController.cs
@@ -1,18 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using Pomodoro.Di;
 using Pomodoro.Di.Provider;
 
 namespace Pomodoro.Service.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class PomodoroController: ControllerBase
+    public class PomodoroController : ControllerBase
     {
         private readonly IPomodoroProvider _provider;
 
         public PomodoroController(IPomodoroProvider provider)
         {
             _provider = provider;
+        }
+
+        [HttpGet(Name = "GetAllPomodoros")]
+        public IEnumerable<IPomodoroData> GetAll()
+        {
+            return _provider.GetAll().Result;
+        }
+
+        [HttpGet(Name = "GetPomodoroByID")]
+        public IPomodoroData GetByID(Guid guid)
+        {
+            return _provider.GetById(guid);
+        }
+
+        [HttpPost(Name = "DeletePomodoro")]
+        public void Delete(Guid guid)
+        {
+            _provider.Delete(guid);
+        }
+
+        [HttpPost(Name = "CreatePomodoro")]
+        public IPomodoroData Create(IPomodoroData pomodoroData)
+        {
+            _provider.Create(pomodoroData);
+            return _provider.GetById(pomodoroData.Id);
+        }
+
+        [HttpPost(Name = "UpdatePomodoro")]
+        public IPomodoroData Update(IPomodoroData pomodoroData)
+        {
+            _provider.Update(pomodoroData);
+            return _provider.GetById(pomodoroData.Id);
         }
     }
 }
