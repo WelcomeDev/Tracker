40e6fd4d4823a5cdee021264aa84d0e6c4d3d88a
galoperidol08@gmail.com <galoperidol08@gmail.com>
2021-11-09 23:26:39 +1000

    add: unit test db

---
+++ PomodoroTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pomodoro.Bll.Provider.MySql.Entity;
+using System.Linq;
+
+namespace PomodoroTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            PomodoroContext context = new PomodoroContext();
+            context.Add(new User()
+            {
+                Id = new System.Guid(),
+                Name = "tester"
+            });
+
+            var userCount = context.Users.Count();
+            Assert.AreEqual(userCount, 1);
+        }
+    }
+}
