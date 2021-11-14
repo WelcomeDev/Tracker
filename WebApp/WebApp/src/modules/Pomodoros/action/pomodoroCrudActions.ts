21414c37279fc7cd60ce2a8cc192b86d15075950
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-11-15 09:42:37 +1000

    add: on CORS error try fix

--- WebApp/WebApp/src/modules/Pomodoros/action/pomodoroCrudActions.ts
+++ WebApp/WebApp/src/modules/Pomodoros/action/pomodoroCrudActions.ts
@@ -10,15 +10,18 @@ const SERVICE_URL = `${globalConfig.baseURL}/pomodoro`
 export function getAll(): Promise<Pomodoro[]> {
     console.log('on get all')
     return axios.get<Pomodoro[]>(SERVICE_URL, {
-                    headers: {
-                        "access-control-allow-origin": 'https://localhost',
-                    },
-                })
-                .then(x => {
-                    console.log(x);
-                    return x.data
-                    // x.data.forEach(x => console.log(x));
-                });
+        headers: {
+            "access-control-allow-origin": '*',
+            "access-control-allow-headers": "Origin, X-Requested-With, Content-Type, Accept",
+            "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "access-control-allow-credentials": "true",
+        },
+    })
+        .then(x => {
+            console.log(x);
+            return x.data
+            // x.data.forEach(x => console.log(x));
+        });
     // client.get<any>(SERVICE_URL)
     //       .then(x => {
     //           console.log(x);
@@ -30,25 +33,25 @@ export function getAll(): Promise<Pomodoro[]> {
 export function get(id: string): Promise<Pomodoro> {
     const currentUrl = `${SERVICE_URL}/${id}`;
     return client.get<Pomodoro>(currentUrl)
-                 .then(x => x.data);
+        .then(x => x.data);
 }
 
 export function create(pomodoroData: PomodoroEssentialsDto): Promise<Pomodoro> {
     const CREATE_URL = `${SERVICE_URL}/create`;
     return client.post<Pomodoro>(CREATE_URL, pomodoroData)
-                 .then(x => x.data);
+        .then(x => x.data);
 }
 
 
 export function update(id: string, pomodoroData: PomodoroEssentialsDto): Promise<Pomodoro> {
     const currentUrl = `${SERVICE_URL}/${id}/update`;
     return client.post<Pomodoro>(currentUrl, pomodoroData)
-                 .then(x => x.data);
+        .then(x => x.data);
 }
 
 //todo: check statu scode
 export function remove(id: string) {
     const currentUrl = `${SERVICE_URL}/${id}/delete`;
     client.post<Pomodoro>(currentUrl)
-          .then(x => console.log(x.status));
+        .then(x => console.log(x.status));
 }
