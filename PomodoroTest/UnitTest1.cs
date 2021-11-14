d5647ad48246f5c75a597bb3d0d204094fc467b7
galoperidol08@gmail.com <galoperidol08@gmail.com>
2021-11-14 20:15:24 +1000

    add: new unit test

--- PomodoroTest/UnitTest1.cs
+++ PomodoroTest/UnitTest1.cs
@@ -5,20 +5,58 @@ using System.Linq;
 namespace PomodoroTest
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTestSQL
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckUser()
         {
             PomodoroContext context = new PomodoroContext();
-            context.Add(new User()
-            {
-                Id = new System.Guid(),
-                Name = "tester"
-            });
-            context.SaveChanges();
             var userCount = context.Users.Count();
             Assert.AreEqual(userCount, 1);
+        }
+
+        [TestMethod]
+        public void CheckAddPomodoro()
+        {
+            using PomodoroContext context = new PomodoroContext();
+            using var transaction = context.Database.BeginTransaction();
+
+            try
+            {
+                var testPomodoro = new Pomodoro.Bll.Provider.MySql.Entity.Pomodoro()
+                {
+                    User = context.Users.First(),
+                    RestDuration = new Duration()
+                    {
+                        Hours = 0,
+                        Minutes = 10
+                    },
+                    WorkDuration = new Duration()
+                    {
+                        Hours = 0,
+                        Minutes = 30
+                    },
+                    Title = "testPomodoro2"
+                };
+
+                context.Add(testPomodoro);
+                context.SaveChanges();
+
+                var pomodoroGUID = testPomodoro.Id;
+
+                Assert.AreEqual(testPomodoro, context.Pomodoros.First(x => x.Id == pomodoroGUID));
+
+                context.Remove(testPomodoro);
+                context.SaveChanges();
+
+                transaction.Commit();
+            }
+            catch
+            {
+
+            }
+
+            
         }
     }
 }
